2fe1b296defdd2e64f1a252d1b3c31554c74f450
WorldMir04 <yashnarbek6033@gmail.com>
2023-03-06 23:28:03 +0500

    Created ProfileRepo

---
+++ src/main/java/repository/ProfileRepo.java
@@ -0,0 +1,8 @@
+package repository;
+
+public class ProfileRepo {
+    String url = "jdbc:postgresql://localhost:5432/atto_full";
+    String dbUser="postgres";
+    String dbPassword="Yashka_04";
+
+}
