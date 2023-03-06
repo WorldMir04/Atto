6a850f8494308605c0ab9095ee1d32f1d099a6ee
WorldMir04 <yashnarbek6033@gmail.com>
2023-03-07 00:02:11 +0500

    checkParam  method in ProfileRepo

--- src/main/java/repository/ProfileRepo.java
+++ src/main/java/repository/ProfileRepo.java
@@ -1,10 +1,12 @@
 package repository;
 
 import dto.Profile;
+import entity.Result;
 import enums.Role;
 
 import java.sql.*;
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class ProfileRepo {
     String url = "jdbc:postgresql://localhost:5432/atto_full";
@@ -42,5 +44,49 @@ public class ProfileRepo {
 
     }
 
+
+    private Result checkParam(Profile profile) {
+        if (Objects.isNull(profile)) {
+            return new Result("Data was required", false);
+        }
+        String name = profile.getName();
+
+        if (Objects.isNull(name) || name.isBlank()) {
+            return new Result("Name was requiret", false);
+        }
+        String phone = profile.getPhone();
+        if (Objects.isNull(phone) || phone.isBlank()) {
+            return new Result("Phone number is required", false);
+        }
+        if (!phone.matches("\\+998\\d{9}")) {
+            return new Result("Phone number is invalid", false);
+        }
+        try {
+            Class.forName("org.postgres.Driver");
+            Connection connection = DriverManager.getConnection(url, dbUser, dbPassword);
+            String sql = "select * from profile where phone=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, profile.getPhone());
+            ResultSet resultSet = preparedStatement.executeQuery();
+            int counter = 0;
+
+            while (resultSet.next()) {
+                counter += resultSet.getInt(1);
+
+            }
+
+            if (counter > 0) {
+                return new Result("This number already exists", false);
+
+            }
+            return new Result("Checked ", true);
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            return new Result("Error", false);
+
+        }
+
+    }
 
 }
