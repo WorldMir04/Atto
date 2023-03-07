2c5f1b136bcd11600206cfe8357dfae56d87a734
WorldMir04 <yashnarbek6033@gmail.com>
2023-03-07 12:21:19 +0500

    createcard method from CardRepo

--- src/main/java/repository/CardRepo.java
+++ src/main/java/repository/CardRepo.java
@@ -3,15 +3,34 @@ package repository;
 import db.DataBase;
 import entity.Result;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class CardRepo {
-//public Result createCard(String number , String exp_date){
-//}
+    public Result createCard(String number, String exp_date) {
+
+        try {
+            Result result = checkParam(number, exp_date);
+            if (!result.isSuccess()) {
+
+                return new Result(result.getMessage(), false);
+
+            }
+
+            Connection connection = DataBase.getConnection();
+            String sql = "insert into card(number,exp_date,created_date) values(?,?,?) ";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, "number");
+            preparedStatement.setString(2, "exp_date");
+            preparedStatement.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now()));
+            preparedStatement.execute();
+            return new Result("Card successfully created", true);
+        } catch (SQLException e) {
+            return new Result("error in server", false);
+        }
+    }
 
     public Result checkParam(String number, String exp_date) {
         if (Objects.isNull(number) || number.isBlank()) {
